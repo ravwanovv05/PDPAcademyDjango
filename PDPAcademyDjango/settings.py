3bffb88bc032695705d5ae362738ebab52d2651f
Samidillo Ravshanov <samidilloravshanov14gmail.com>
2024-09-25 10:52:48 +0500

    database settings have been configured

--- PDPAcademyDjango/settings.py
+++ PDPAcademyDjango/settings.py
@@ -70,8 +70,12 @@ WSGI_APPLICATION = 'PDPAcademyDjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'helper_bot.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
